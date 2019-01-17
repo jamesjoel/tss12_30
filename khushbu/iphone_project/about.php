36b32be515a20a278cb3043237daf40a681eb41d
khushburaghu <khushi.raghu.kr@gmail.com>
2019-01-17 18:39:12 +0530

    my first commit

---
+++ khushbu/iphone_project/about.php
@@ -0,0 +1,26 @@
+<?php
+include("header1.php");
+?>
+<?php
+include("menu2.php");
+?><html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+			<div id="right-content-bottom">
+				<h3>This is about page</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include("footer1.php");
+?>
---
+++ khushbu/iphone_project/menu.php
@@ -0,0 +1,12 @@
+<div id="menu">
+		<div id="inside-menu">
+			<div id="inside-menu">
+		<ul>
+			<li><a href="index.php" class="first-hover">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+			<li><a href="#">Help</a></li>
+		</ul>
+	</div>
+		</div>
+	</div>
