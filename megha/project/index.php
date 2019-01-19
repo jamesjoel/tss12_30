950c405ed542e3687fd563b0f6854eff902bb5e7
meghasahu9 <46006466+meghasahu9@users.noreply.github.com>
2019-01-19 13:48:50 +0530

    i have done signup page

---
+++ megha/project/index.php
@@ -0,0 +1,113 @@
+<?php
+include("header.php");
+?>
+<div id="content">
+	<div id="in-content">
+		<div id="right-content">
+			<div class="content-box">
+				<div class="image">
+					<img src="images/2.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<div class="price"><h4>$200</h4></div>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/3.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$100</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/4.jpeg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$300</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/5.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$300</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/6.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$150</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/7.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$250</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/8.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$180</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/9.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$350</h4>
+				</div>
+			</div>
+			<div class="content-box">
+				<div class="image">
+					<img src="images/1.jpg" height="135" width="150"/>
+				</div>
+				<div class="price">
+					<div class="add-to-card">add to card</div>
+					<h4>$500</h4>
+				</div>
+			</div>
+		</div>
+		<div id="left-content">
+			<div id="left-menu">
+				<h3>Products</h3>
+				<ul>
+					<li><a href="#">Women handbags</a></li>
+					<li><a href="#">Men</a></li>
+					<li><a href="#">Kids</a></li>
+					<li><a href="#">Winter Store</a></li>
+					<li><a href="#">Clothing</a></li>
+					<li><a href="#">Watches</a></li>
+					<li><a href="#">Footwear</a></li>
+					<li><a href="#">Jewellery</a></li>
+					<li><a href="#">Toys</a></li>
+					<li><a href="#">Fragrances</a></li>
+					<li><a href="#">Beauty Wellness</a></li>
+					<li><a href="#">School Supplies</a></li>
+					<li><a href="#">Eyewear</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include("footer.php");
+?>
