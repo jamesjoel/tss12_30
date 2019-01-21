6eb3495c8a682edd60c697ddb35ddaf341390463
khushburaghu <khushi.raghu.kr@gmail.com>
2019-01-21 17:54:20 +0530

    new folder

---
+++ khushbu_new/iphone_project/adminpannel/add_category.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php");
+?>	
+<div id="content">
+	<div id="inside-content">
+	<h2>ADD CATEGORY</h2>
+	<hr>
+	<form action="save1_product.php" method="post">
+	<div id="login-box">
+	<table align="center">
+		<tr>
+			<td>addcategory</td>
+			<td><input type="Text" name="product_category" class="input"/></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center">
+				<input type="submit" value="Add" class="btn">
+			</td>
+		</tr>
+	</table>
+</div>
+</form>
+</div>
+</div>
