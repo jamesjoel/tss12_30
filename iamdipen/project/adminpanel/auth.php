b9c24c92b36e6794fb476df0b672b5bc26fb8327
iamdipen <dipendrasinghpawar@gmail.com>
2019-02-01 15:14:47 +0530

    1feb nothing new

--- iamdipen/project/adminpanel/auth.php
+++ iamdipen/project/adminpanel/auth.php
@@ -1,21 +1,33 @@
 <?php
-include("connect.php");
-
-$u = $_POST['username'];
+// print_r($_POST);
+include("../connect.php");
+$u = $_POST['admin_name'];
 $p = $_POST['pass'];
+// echo $u;
+// echo $p;
 
- $query = "SELECT * FROM user WHERE username ='$u'";
+$query = "SELECT * FROM admin WHERE admin_name ='$u'";
+$result = mysqli_query($con,$query);
+if (mysqli_num_rows($result)==1)
+{
+ $data_admin= mysqli_fetch_assoc($result);
+    if($data_admin["pass"]==md5($p))
+    {
+//    echo "hello";
+      $_SESSION["name"]=$data['admin_name'];
+      header("location:header.php");
+    }
 
- $result = mysqli_query($con, $query);
- print_r($result);
+    else
+    {
+    echo"invalid";
+    // header("location:login.php");
+    }
 
- if(mysqli_num_rows($result)==1) 
- {
-    $data= mysqli_fetch_assoc($result)	
- }
- else
- {
-    header(location:index.php);
- }
+}
+else
+ { 
+   // header("location:login.php");
+ }  
 
 ?>
--- iamdipen/project/connect.php
+++ iamdipen/project/connect.php
@@ -1,3 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","root","password","dipendra") or die("not connect.");
+$con=mysqli_connect("localhost","root","password","dipendra");
+session_start();
 ?>
