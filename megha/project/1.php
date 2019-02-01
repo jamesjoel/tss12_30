cb53a6ce94fa5c4a462c92a22c390d25e842afde
meghasahu9 <46006466+meghasahu9@users.noreply.github.com>
2019-02-01 15:30:10 +0530

    working on

---
+++ megha/project/1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="2.php" method="get">
+	First Name <input type="text" name="first_name" />
+	<br />
+	<br />
+	Last Name <input type="text" name="last_name" />
+	<br />
+	<Br />
+	<input type="submit" value="Submit">
+
+</form>
+</body>
+</html>
