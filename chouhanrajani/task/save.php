d1bb367f78229868e37f62e3c1eb45d8b2e4163e
rajanichouhan <rajnichouhan.indore@gmail.com>
2019-02-03 21:16:55 +0530

    task is working ..rajani

--- chouhanrajani/task/save.php
+++ chouhanrajani/task/save.php
@@ -1,12 +1,14 @@
 <?php
-$con=mysqli_connect("localhost","root","password","task");
+// print_r($_POST);
+// die;
+$con=mysqli_connect("localhost","root","","task");
 $a=$_POST['title'];
 $b=$_POST['subject'];
 $c=$_POST['msg'];
 
 
 
-$query="INSERT INTO blog(title,subject,message) VALUES('$a','$b','$c')";
+$query="INSERT INTO blog(field,subject,message) VALUES('$a','$b','$c')";
 mysqli_query($con,$query);
 header("location:blog.php");
 
