1c321f383df10b333c2b9c22b3956c2c2677c856
jamesjoel <james.joelj@gmail.com>
2019-02-11 14:20:57 +0530

    javascript day 1

---
+++ js/day1_a.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+		function demo()
+		{
+			// var a = document.getElementById("para").innerHTML;
+
+			document.getElementById("para").innerHTML="hello world";
+			// alert(a);
+		}
+	</script>
+
+</head>
+<body>
+<button onclick="demo()">ok</button>
+<p id="para"></p>
+</body>
+</html>
