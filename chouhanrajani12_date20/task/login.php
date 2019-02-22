589a07520e4b5149ad4e2757cd8eb0e51bf7390a
rajanichouhan <rajanichouhan>
2019-02-22 14:40:34 +0530

    done

---
+++ chouhanrajani12_date20/task/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="in-contant">
+	<form action="login_auth.php" method="post">
+		<table align="center" border="1">
+			<tr><td>username</td>
+				<td><input type="text" name="username"></td></tr>
+			<tr><td>password</td>
+				<td><input type="password" name="pass"></td>
+			</tr>
+			<tr><td colspan="2" align="center"><input type="submit" value="submit"></td></tr>
+		</table>
+	</form>
+</div>
+</body>
+</html>
---
+++ chouhanrajani12_date20/task/login_auth.php
@@ -0,0 +1,21 @@
+<<?php 
+$con=mysqli_connect("localhost","root","password","rajni");
+// print_r($_POST);
+// die;
+$a=$_POST['username'];
+$b=$_POST['pass'];
+$query="SELECT * FROM user WHERE username=$a";
+$result=mysqli_query($con,$query);
+
+if(mysqli_num_rows($result)==1){
+
+$data=mysqli_fetch_assoc($result);
+if($data["password"]==$b)
+{
+	echo "yes";
+}
+else{
+	echo "no";
+}
+}
+?>
