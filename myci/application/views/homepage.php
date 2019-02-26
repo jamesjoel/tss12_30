e851fb50147268924d87906d10838135220a0c20
jamesjoel <james.joelj@gmail.com>
2019-02-26 14:42:31 +0530

    codeigniter day1

---
+++ myci/application/views/homepage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body{
+			background-color: #225588;
+		}
+	</style>
+</head>
+<body>
+<h1>Home Page</h1>
+</body>
+</html>
