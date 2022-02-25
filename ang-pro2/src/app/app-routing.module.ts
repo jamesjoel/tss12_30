018a21ee08d05c8e624eb68eb34d48b1d942eb91
jamesjoel <jamesjoelj@gmail.com>
2022-02-25 12:40:45 +0530

    angular day2

--- ang-pro2/src/app/app-routing.module.ts
+++ ang-pro2/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HelpComponent } from './pages/help/help.component';
+
+
+const routes: Routes = [
+  {
+    path : "help",
+    component : HelpComponent
+  },
+  {
+    path : "about",
+    component : AboutComponent
+  },
+  {
+    path : "contact",
+    component : ContactComponent
+  },
+  {
+    path : "",
+    component : HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ang-pro2/src/app/app.module.ts
+++ ang-pro2/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { Box1Component } from './shared/box1/box1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HelpComponent } from './pages/help/help.component';
+import { InfoComponent } from './shared/info/info.component';
+import { BottomHeaderComponent } from './shared/bottom-header/bottom-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Box1Component,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HelpComponent,
+    InfoComponent,
+    BottomHeaderComponent
   ],
   imports: [
     BrowserModule,
