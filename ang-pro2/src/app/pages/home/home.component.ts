cf70cdd3e281cce4fea85f37f22ede16e12e6dbc
jamesjoel <jamesjoelj@gmail.com>
2022-02-26 12:47:51 +0530

    event in angular -1

--- ang-pro2/src/app/pages/home/home.component.ts
+++ ang-pro2/src/app/pages/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  a=0;
+  b=0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  demo(){
+    this.a = Math.random()*500;
+    this.a = Math.floor(this.a); 
+    // any random number in a lessthen 500
+    this.b = Math.random()*800;
+    this.b = Math.floor(this.b);
+    // any random number in b lessthen 800
+  }
 }
